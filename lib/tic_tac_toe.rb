8108ce1edfb66b4b7a5fda4b8fa0d8b7510c855f
gdilthey <george.dilthey@gmail.com>
2021-01-20 03:08:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,14 +117,14 @@ def play(board)
     counter += 1
     if over?(board)
       break
-      if won?(board)
-        winner = winner(board)
-        puts 'Congratulations, #{winner}!'
-        break
-      elsif draw?(board)
-        puts 'It was a draw!'
-        break
-      end
+    end
+    if won?(board)
+      winner = winner(board)
+      puts 'Congratulations, #{winner}!'
+      break
+    elsif draw?(board)
+      puts 'It was a draw!'
+      break
     end
   end
 end
